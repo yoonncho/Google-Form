3004e7bc498c0b60358e42881c2591c7dd68578b
yoon <nicealice99@gmail.com>
2021-12-23 22:48:17 +0900

    [chore]: react-redux and redux-toolkit 설치

--- src/index.tsx
+++ src/index.tsx
@@ -5,13 +5,17 @@ import reportWebVitals from './reportWebVitals';
 import { GlobalStyle } from './styles/globalStyle';
 import { theme } from './styles/theme';
 import { ThemeProvider } from 'styled-components';
+import store from './store/configureStore';
+import { Provider } from 'react-redux';
 
 ReactDOM.render(
   <React.StrictMode>
-    <ThemeProvider theme={theme}>
-      <GlobalStyle />
-      <App />
-    </ThemeProvider>
+    <Provider store={store}>
+      <ThemeProvider theme={theme}>
+        <GlobalStyle />
+        <App />
+      </ThemeProvider>
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root'),
 );
