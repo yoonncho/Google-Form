c4b8e04ed95725c2c8f75b4f3a377d5ec9a175f8
yoon <nicealice99@gmail.com>
2021-12-24 22:42:58 +0900

    [chore]: icons 추가

---
+++ src/assets/index.ts
@@ -0,0 +1,4 @@
+export { default as TrashIcon } from './icon/trash.svg';
+export { default as CopyIcon } from './icon/copy.svg';
+export { default as ShowIcon } from './icon/show.svg';
+export { default as AddIcon } from './icon/add.svg';
